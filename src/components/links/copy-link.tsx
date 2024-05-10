56434aa2307de187abaf9c20f679e7012627f2a6
tontan2545 <tontan2545@gmail.com>
2024-05-10 17:34:16 +0700

    feat: location origin

--- src/components/links/copy-link.tsx
+++ src/components/links/copy-link.tsx
@@ -12,7 +12,7 @@ interface CopyLinkProps {
 
 const CopyLinkDropdown = (props: CopyLinkProps) => {
   const [, copy] = useCopyToClipboard();
-  const url = "https://slug.vercel.app";
+  const url = window.location.origin;
 
   const handleCopy = (text: string) => () => {
     copy(text)
